aaecf099650f324300316a9c1cc1f5945aa5be5e
Coby <coby5502@gmail.com>
2024-09-24 16:57:52 +0900

    Initial Commit

---
+++ RunDream/RunDreamApp.swift
@@ -0,0 +1,17 @@
+//
+//  RunDreamApp.swift
+//  RunDream
+//
+//  Created by Coby on 9/24/24.
+//
+
+import SwiftUI
+
+@main
+struct RunDreamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
